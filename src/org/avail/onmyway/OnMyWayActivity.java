c8478fe252a5e9e98485c2dbcb5328321193b27a
Yura Zenevich <yura.zenevich@gmail.com>
2012-09-01 01:06:15 -0400

    Inital commit with deployable app.

---
+++ src/org/avail/onmyway/OnMyWayActivity.java
@@ -0,0 +1,21 @@
+package org.avail.onmyway;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import org.apache.cordova.*;
+
+public class OnMyWayActivity extends DroidGap {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_on_my_way, menu);
+        return true;
+    }
+}
