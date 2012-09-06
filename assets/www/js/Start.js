f06d159f28577e2e203fe58792af5ce9068310f3
Yura Zenevich <yura.zenevich@gmail.com>
2012-09-06 00:22:24 -0400

    Working geocoding

--- assets/www/js/Start.js
+++ assets/www/js/Start.js
@@ -6,11 +6,11 @@
         gradeNames: ["autoInit", "fluid.rendererComponent"],
         selectors: {
             locationButton: ".omwc-start-locationButton",
-            locationInput: ".omwc-start-locationButton"
+            locationInput: ".omwc-start-locationInput"
         },
         styles: {
             locationButton: "omw-start-locationButton",
-            locationInput: "omw-start-locationButton"
+            locationInput: "omw-start-locationInput"
         },
         protoTree: {
             locationInput: {
@@ -20,7 +20,68 @@
                 }
             }
         },
+        components: {
+            geocoding: {
+                type: "omw.geocoding",
+                container: "{omw.start}.dom.locationInput"
+            }
+        },
         renderOnInit: true
     });
+
+    fluid.defaults("omw.geocoding", {
+        gradeNames: ["autoInit", "fluid.viewComponent"],
+        components: {
+            geocodeSource: {
+                type: "omw.dataSource.URL",
+                options: {
+                    url: "https://maps.googleapis.com/maps/api/geocode/json?address=%address&sensor=true",
+                    termMap: {
+                        address: "%address"
+                    }
+                }
+            }
+        },
+        minSize: 4,
+        delay: 500
+    });
+
+    omw.geocoding.preInit = function (that) {
+        that.getGeocode = function (address) {
+            that.geocodeSource.get({
+                address: address
+            }, function (data) {
+                that.applier.requestChange("results", data);
+                console.log(JSON.stringify(data));
+                delete that.searching;
+                if (that.address) {
+                    that.getGeocode(that.address);
+                    delete that.address;
+                }
+            });
+        };
+        that.applier.modelChanged.addListener("address", function () {
+            if (that.searching) {
+                that.address = that.model.address;
+                return;
+            }
+            that.getGeocode(that.model.address);
+        });
+    };
+
+    omw.geocoding.finalInit = function (that) {
+        that.container.keydown(function () {
+            clearTimeout(that.outFirer);
+            that.outFirer = setTimeout(function () {
+                var address = that.container.val();
+                if (address.lenth < minSize) {
+                    return;
+                }
+                if (address !== that.model.address) {
+                    that.applier.requestChange("address", address);
+                }
+            }, that.options.delay);
+        });
+    };
 
 })(fluid, jQuery);
